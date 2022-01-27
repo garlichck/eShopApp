e38a988abeae5596a7d4354872e91bd5b617abc9
garlichck <garlichck@hotmail.com>
2022-01-27 13:25:48 +0800

    [eShopApp]-Add header to page.

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-     eShop Website Test 3
+      <Header></Header>
     </div>
   );
 }
